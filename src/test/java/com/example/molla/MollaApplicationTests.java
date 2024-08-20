81458a5b16db4198acca2609826c39ad31a5fe46
ParkHyunS00 <parkhyuns00@naver.com>
2024-08-20 23:20:27 +0900

    First commit

---
+++ src/test/java/com/example/molla/MollaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.molla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MollaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
