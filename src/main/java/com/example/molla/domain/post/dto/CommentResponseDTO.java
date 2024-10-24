166dd9ae7f7406cdfe901c6e2833b1cfbab19c52
ParkHyunS00 <parkhyuns00@naver.com>
2024-10-24 22:55:51 +0900

    [fix] 댓글 조회시 생성날짜 반환하도록 수정

--- src/main/java/com/example/molla/domain/post/dto/CommentResponseDTO.java
+++ src/main/java/com/example/molla/domain/post/dto/CommentResponseDTO.java
@@ -1,6 +1,7 @@
 package com.example.molla.domain.post.dto;
 
 import com.example.molla.domain.post.domain.Comment;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
@@ -11,8 +12,9 @@ public class CommentResponseDTO {
     private Long id;
     private String content;
     private String username;
+    private LocalDateTime createdDate;
 
     public static CommentResponseDTO fromEntity(Comment comment) {
-        return new CommentResponseDTO(comment.getId(), comment.getContent(), comment.getUser().getUsername());
+        return new CommentResponseDTO(comment.getId(), comment.getContent(), comment.getUser().getUsername(), comment.getCreateDate());
     }
 }
